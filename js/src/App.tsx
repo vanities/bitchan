78b0276d4fd6bd719145dca662fc4c71856fc465
Adamant Mischke <mischkeaa@gmail.com>
2020-05-22 01:07:36 -0500

    workign!

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Web3 from 'web3';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import TopNav from './Nav';
+import * as User  from './contracts/User.json';
+//import { SignUp } from './SignUp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Board() {
+  return <h3>Board</h3>;
 }
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { numUsers: 0 };
+    this.Home = this.Home.bind(this);
+  }
+
+  componentWillMount() {
+    this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+    // TODO: max this contract Address automatic
+    const contractAddress = '0x695f57efefbf06623ea23bffc43909b2e268c5f4';
+    const UserContract = new web3.eth.Contract(User.abi, contractAddress);
+    const numUsers = await UserContract.methods.getNumUsers().call();
+    this.state = { numUsers: numUsers };
+  }
+
+  Home() {
+    return <h3>Number of users: {this.state.numUsers}</h3>;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <TopNav />
+            <Switch>
+              <Route exact path="/">
+                <this.Home />
+              </Route>
+              <Route exact path="/board">
+                <Board />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
