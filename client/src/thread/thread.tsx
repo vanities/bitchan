d7b206c3b89cafda214528ac50a7dbb13ae565da
Adamant Mischke <mischkeaa@gmail.com>
2020-07-07 23:11:24 -0500

    fix thread replies, add thread css for op

--- client/src/thread/thread.tsx
+++ client/src/thread/thread.tsx
@@ -1,24 +1,39 @@
 import * as React from "react";
-import { Card, CardBody, CardTitle, CardText, CardImg } from "reactstrap";
+import { Card, CardBody, CardTitle, CardText, CardImg, Col } from "reactstrap";
+
+import "./thread.css";
 
 import { drizzleReactHooks } from "@drizzle/react-plugin";
 
-export function getReplies (call, indexFirstReply) {
+export function getReplies (call, indexLastReply) {
   const replies = [];
-  var nextIndex = indexFirstReply;
-  while (true) {
-    var reply = call("Bitchan", "replies", nextIndex);
-    if (reply === undefined) {
-      break;
+  for (let replyIndex = 1; replyIndex < indexLastReply; replyIndex++) {
+    var reply = call("Bitchan", "replies", replyIndex);
+    if (reply) {
+      replies.push(reply);
     }
-    nextIndex = reply[3];
-    replies.push(reply);
   }
   return replies;
 }
 
 interface ThreadProps {
   threadId: number;
+}
+
+export function replyCard (media, text, timestamp) {
+  return (
+    <Col sm="6">
+      <Card>
+        <CardImg top width="100%" src={media} alt="Card image cap" />
+        <CardBody>
+          <CardText>{text}</CardText>
+          <CardText>
+            <small className="text-muted">{timestamp}</small>
+          </CardText>
+        </CardBody>
+      </Card>
+    </Col>
+  );
 }
 
 export function Thread (props, context) {
@@ -30,18 +45,27 @@ export function Thread (props, context) {
   const text = originalPoster ? originalPoster[1] : "loading";
   const media = originalPoster ? originalPoster[2] : "loading";
   const indexFirstReply = originalPoster ? originalPoster[4] : "loading";
+  const indexLastReply = originalPoster ? originalPoster[5] : "loading";
   const _threadId = originalPoster ? originalPoster[5] : "loading";
   const epochTime = originalPoster ? originalPoster[6] : 0;
   var timePosted = new Date(0); // The 0 there is the key, which sets the date to the epoch
   timePosted.setUTCSeconds(epochTime);
 
   const replies = useCacheCall(["Bitchan"], (call) =>
-    getReplies(call, indexFirstReply)
+    getReplies(call, indexLastReply)
   );
+
+  console.log(replies);
 
   const opCard = (
     <Card>
-      <CardImg top width="100%" src={media} alt="Card image cap" />
+      <CardImg
+        top
+        width="40%"
+        src={media}
+        alt="Card image cap"
+        className="opcard"
+      />
       <CardBody>
         <CardTitle>{title}</CardTitle>
         <CardText>{text}</CardText>
@@ -55,30 +79,8 @@ export function Thread (props, context) {
   return (
     <div>
       <div>{opCard}</div>
-
-      {replies.map((reply) => (
-        <div>
-          <Card>
-            <CardImg
-              top
-              width="100%"
-              src="/assets/318x180.svg"
-              alt="Card image cap"
-            />
-            <CardBody>
-              <CardTitle>Card Title</CardTitle>
-              <CardText>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </CardText>
-              <CardText>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </CardText>
-            </CardBody>
-          </Card>
-        </div>
-      ))}
+      <div> {/* postReplyButton */} </div>
+      {replies.map((reply) => replyCard(reply[1], reply[0], reply[4]))}
     </div>
   );
 }
