62b6fa182e026fcf9f0d4f95767601c3936fbb11
Adamant Mischke <mischkeaa@gmail.com>
2020-07-09 00:02:34 -0500

    test deploying to ropsten

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const infuraKey = process.env.INFURA_KEY;
+
+const fs = require("fs");
+const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   /**
@@ -43,9 +43,9 @@ module.exports = {
     // options below to some value.
 
     development: {
-      host: 'ganache', // Localhost (default: none)
+      host: "ganache", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
-      network_id: '*', // Any network (default: none)
+      network_id: "*", // Any network (default: none)
       gas: 2000000,
       gasPrice: 10000000000,
     },
@@ -62,14 +62,18 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/${INFURA_KEY}`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for private networks
     // private: {
@@ -95,7 +99,7 @@ module.exports = {
           enabled: true,
           //    runs: 200
         },
-        evmVersion: 'byzantium',
+        evmVersion: "byzantium",
       },
     },
   },
